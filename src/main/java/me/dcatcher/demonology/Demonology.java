6072e12aa819ab2cea45fc86662e979f9c8156cd
DCatcher <dan@dcatcher.me>
2018-03-30 18:15:21 +0100

    It Begins

--- src/main/java/me/dcatcher/demonology/Demonology.java
+++ src/main/java/me/dcatcher/demonology/Demonology.java
@@ -1,4 +1,31 @@
 package me.dcatcher.demonology;
 
-public class Demonology {
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Demonology.MODID, name = Demonology.NAME, version = Demonology.VERSION)
+public class Demonology
+{
+    public static final String MODID = "demonology";
+    public static final String NAME = "Demonology";
+    public static final String VERSION = "0.01";
+
+    private static Logger logger;
+
+    @Mod.Instance(Demonology.MODID)
+    public static Demonology instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
 }
