dfc82f98b0e2e6ec2eeef5d461ec707efea5d170
DCatcher <dan@dcatcher.me>
2018-03-30 22:12:29 +0100

    Finished for the night

--- src/main/java/me/dcatcher/demonology/block/BlockBasicAltar.java
+++ src/main/java/me/dcatcher/demonology/block/BlockBasicAltar.java
@@ -1,9 +1,12 @@
 package me.dcatcher.demonology.block;
 
 import me.dcatcher.demonology.tileentities.TileEntityBasicAltar;
+import net.minecraft.block.BlockChest;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -20,11 +23,16 @@ public class BlockBasicAltar extends BlockTileEntity<TileEntityBasicAltar> {
     }
 
     @Override
-    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        if (!worldIn.isRemote) {
-            TileEntityBasicAltar te = this.getTileEntity(worldIn, pos);
-            te.count++;
-            playerIn.sendMessage(new TextComponentString("Count is now " + te.count));
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (!world.isRemote) {
+            TileEntityBasicAltar te = this.getTileEntity(world, pos);
+            if (player.inventory.getCurrentItem() == ItemStack.EMPTY) {
+                // empty hand - trigger the crafting
+//                InventoryHelper.dropInventoryItems(world, pos, te.iStackHandler); todo
+            } else {
+                // place it on the altar
+
+            }
         }
         return true;
     }
--- src/main/java/me/dcatcher/demonology/tileentities/TileEntityBasicAltar.java
+++ src/main/java/me/dcatcher/demonology/tileentities/TileEntityBasicAltar.java
@@ -1,5 +1,6 @@
 package me.dcatcher.demonology.tileentities;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
@@ -8,8 +9,7 @@ import net.minecraftforge.items.ItemStackHandler;
 public class TileEntityBasicAltar extends TileEntity {
 
     public ItemStackHandler iStackHandler;
-
-    public int count = 0;
+    public int itemCount = 0;
 
     public TileEntityBasicAltar() {
         iStackHandler = new ItemStackHandler(6);
@@ -19,6 +19,7 @@ public class TileEntityBasicAltar extends TileEntity {
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
         super.writeToNBT(compound);
         compound.setTag("items", iStackHandler.serializeNBT());
+        compound.setInteger("itemCount", itemCount);
         return compound;
     }
 
@@ -28,6 +29,13 @@ public class TileEntityBasicAltar extends TileEntity {
         if (compound.hasKey("items")) {
             iStackHandler.deserializeNBT((NBTTagCompound) compound.getTag("items"));
         }
+        if (compound.hasKey("itemCount")) {
+            itemCount = compound.getInteger("itemCount");
+        }
+    }
+
+    public void placeItem(Item item) {
+        iStackHandler.setStackInSlot(itemCount++, new ItemStack(item, 1));
     }
 
 }
